de3bb1ff78e9b30ed218df1e73d1fd69e336dfbb
Nicholas Robinia <nicholas.robinia@gmail.com>
2025-08-05 11:27:28 +1000

    implemented pygame groups

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -17,10 +23,15 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
+
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
